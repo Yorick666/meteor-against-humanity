b01dde440d5371160cae3145ac9b3bcf8cca97f7
Randall A. Gordon <randall@randallagordon.com>
2013-12-24 16:23:47 -0800

    v0.0.1...or something

--- client/meteor-humanity.js
+++ client/meteor-humanity.js
@@ -2,17 +2,24 @@ var inGame = function () {
   return Session.get("inGame");
 };
 
-Template.cardList.cards = function () {
+var currentCzar = function () {
   var gameId = Session.get("inGame");
-  if (gameId !== undefined) {
-    return Games.findOne(gameId).players[Meteor.user()._id].hand;
-  }
+  var game = Games.findOne(gameId);
+  return game.currentCzar === Meteor.user()._id;
+};
+
+var waiting = function () {
+  var gameId = Session.get("inGame");
+  var game = Games.findOne(gameId);
+  return game.state === "waiting";
 };
 
 Template.playerList.players = function () {
   var gameId = Session.get("inGame");
   if (gameId) {
-    var players = Object.keys(Games.findOne(gameId).players);
+    var players = Object.keys(Games.findOne(gameId).players).filter(function (p) {
+      return p !== Meteor.user()._id; // Filter out self
+    });
     return Meteor.users.find({_id:{$in:players}}).fetch();
   }
 };
@@ -22,11 +29,13 @@ Template.menu.helpers({
 });
 
 Template.main.helpers({
-  inGame: inGame
+  inGame: inGame,
+  currentCzar: currentCzar
 });
 
 Template.playerList.helpers({
-  inGame: inGame
+  inGame: inGame,
+  waiting: waiting
 });
 
 Template.menu.events({
@@ -54,15 +63,167 @@ Template.menu.events({
     if (gameId) {
       Meteor.call("leaveGame", gameId, function () {
         Session.set("inGame", undefined);
+        Session.set("canSubmit", false);
+        Session.set("submittedAnswer", false);
       });
     }
   }
 });
 
+Template.cardList.cards = function () {
+  var gameId = Session.get("inGame");
+  var hand;
+  if (gameId !== undefined) {
+
+    hand = Games.findOne(gameId).players[Meteor.user()._id].hand;
+    hand.forEach(function (card, i) {
+      card.index = i;
+    });
+
+    return hand;
+  }
+};
+
+Template.cardList.question = function () {
+  var gameId = Session.get("inGame");
+  if (gameId) {
+    return Games.findOne(gameId).question;
+  }
+};
+
+Template.cardList.selected = function (index) {
+  var userId = Meteor.user()._id;
+  var gameId = Session.get("inGame");
+  var game;
+
+  if (gameId) {
+    game = Games.findOne(gameId);
+    if (game.players[userId].hand[index].selected) { return "selected"; }
+  }
+
+  return "";
+};
+
+Template.cardList.canSubmit = function () {
+  return Session.get("canSubmit");
+};
+
+Template.cardList.submittedAnswer = function () {
+  var userId = Meteor.user()._id;
+  var gameId = Session.get("inGame");
+  var game = Games.findOne(gameId);
+
+  return game.players[userId].answer;
+};
+
 Template.cardList.events({
   "click .card": function (e) {
     e.preventDefault();
 
-    $(e.target).toggleClass("selected");
+    if ($(e.target).hasClass("question")) { return; } // Can't select question
+
+    var userId = Meteor.user()._id;
+    var gameId = Session.get("inGame");
+    var game = Games.findOne(gameId);
+    var index = $(e.target).data("index");
+
+    game.players[userId].hand[index].selected = !game.players[userId].hand[index].selected;
+
+    Games.update(gameId, game);
+
+    var numSelected = 0;
+    game.players[userId].hand.forEach(function (card) {
+      if (card.selected) { numSelected++; }
+    });
+
+    if (numSelected === game.question.numAnswers) {
+      game.players[userId].canSubmit = true;
+      Session.set("canSubmit", true);
+    } else {
+      game.players[userId].canSubmit = false;
+      Session.set("canSubmit", false);
+    }
+
   },
+  "click #submit-answer": function (e) {
+    var gameId = Session.get("inGame");
+
+    Meteor.call("submitAnswer", gameId, function (error, id) {
+      Session.set("submittedAnswer", true);
+      Session.set("canSubmit", false);
+    });
+  }
+});
+
+Template.czarList.cards = function () {
+  var gameId = Session.get("inGame");
+  var submittedAnswers;
+  if (gameId) {
+    submittedAnswers = Games.findOne(gameId).submittedAnswers;
+    submittedAnswers.forEach(function (answer, i) { answer.index = i; });
+    return submittedAnswers;
+  }
+};
+
+Template.czarList.question = function () {
+  var gameId = Session.get("inGame");
+  if (gameId) {
+    return Games.findOne(gameId).question;
+  }
+};
+
+Template.czarList.selected = function (index) {
+  var userId = Meteor.user()._id;
+  var gameId = Session.get("inGame");
+  var game;
+
+  if (gameId) {
+    game = Games.findOne(gameId);
+    if (game.submittedAnswers[index][0].selected) { return "selected"; }
+  }
+
+  return "";
+};
+
+Template.czarList.canSubmit = function () {
+  return Session.get("canSubmit");
+};
+
+Template.czarList.events({
+  "click .card": function (e) {
+    e.preventDefault();
+
+    if ($(e.target).hasClass("question")) { return; } // Can't select question
+
+    var userId = Meteor.user()._id;
+    var gameId = Session.get("inGame");
+    var game = Games.findOne(gameId);
+    var index = $(e.target).data("index");
+
+    game.submittedAnswers[index][0].selected = !game.submittedAnswers[index][0].selected;
+
+    Games.update(gameId, game);
+
+    var numSelected = 0;
+    game.submittedAnswers.forEach(function (card) {
+      if (card[0].selected) { numSelected++; }
+    });
+
+    if (numSelected === game.question.numAnswers) {
+      game.players[userId].canSubmit = true;
+      Session.set("canSubmit", true);
+    } else {
+      game.players[userId].canSubmit = false;
+      Session.set("canSubmit", false);
+    }
+
+  },
+  "click #submit-winner": function (e) {
+    var gameId = Session.get("inGame");
+
+    Meteor.call("submitWinner", gameId, function (error, id) {
+      Session.set("submittedWinner", true);
+      Session.set("canSubmit", false);
+    });
+  }
 });
--- collections/games.js
+++ collections/games.js
@@ -13,14 +13,20 @@ Meteor.methods({
 
     game = {
       players: {},
+      numPlayers: 1,
+      numAnswers: 0,
       answers: Cards.find({ cardType: "A", expansion: "Base" }).fetch(),
-      questions: Cards.find({ cardType: "Q", expansion: "Base" }).fetch(),
+      questions: Cards.find({ cardType: "Q", expansion: "Base", numAnswers: 1 }).fetch(),
+      question: undefined,
+      submittedAnswers: [],
+      state: "waiting"
     };
 
     game.answers.shuffle();
     game.questions.shuffle();
 
     game.players[user._id] = { hand: game.answers.splice(0, 10) };
+    game.currentCzar = user._id;
 
     gameId = Games.insert(game);
 
@@ -37,6 +43,12 @@ Meteor.methods({
     if (game.players[user._id]) { return gameId; }
 
     game.players[user._id] = { hand: game.answers.splice(0, 10) };
+    game.numPlayers++;
+
+    if (game.state === "waiting" /* && game.numPlayers > 2 */) {
+      game.state = "playing";
+      game.question = game.questions.shift();
+    }
 
     Games.update(gameId, game);
 
@@ -49,15 +61,90 @@ Meteor.methods({
     if (!user) { throw new Meteor.Error(401, "Must be logged in to leave a new game!"); }
 
     if (game.players[user._id]) {
+      // Put their hand back in the deck
       game.players[user._id].hand.forEach(function (card) { game.answers.push(card); });
       delete game.players[user._id];
-      if (Object.keys(game.players).length === 0) {
+      game.numPlayers--;
+
+      if (Object.keys(game.players).length === 0) { // No one left, remove the game
         Games.remove(game._id);
+      } else if (Object.keys(game.players).length === 1) { // One person left, switch to waiting
+        game.state = "waiting";
+        game.questions.push(game.question); // Put the question back
+        game.question = undefined;
+        Games.update(gameId, game);
       } else {
         Games.update(gameId, game);
       }
     }
 
     return gameId;
+  },
+  dealQuestion: function (gameId) {
+    var game = Games.findOne(gameId);
+    if (game) {
+      game.question = game.questions.shift();
+      Games.update(gameId, game);
+    }
+  },
+  submitAnswer: function (gameId) {
+    var userId = Meteor.user()._id;
+    var game = Games.findOne(gameId);
+    var answer = [], submittedAnswers = [];
+
+    if (game) {
+      game.players[userId].hand.forEach(function (card, i) {
+        if (card.selected) {
+          card.selected = false;
+          answer.push(card); // Add selected card to answer array
+          game.players[userId].hand.splice(i, 1); // Remove from hand
+          game.answers.push(card); // Add it back to the Answers deck
+          game.players[userId].hand.push(game.answers.shift()); // Draw a new card
+        }
+      });
+
+      game.players[userId].answer = answer;
+      game.numAnswers++;
+
+      // Everyone submitted except the Czar?
+      if (game.numAnswers === game.numPlayers - 1) {
+        for (var player in game.players) {
+          if (game.players.hasOwnProperty(player) && game.currentCzar !== player && game.players[player].answer !== undefined) {
+            submittedAnswers.push(game.players[player].answer);
+          }
+        }
+
+        game.state = "submitted";
+        game.submittedAnswers = submittedAnswers;
+      }
+
+      Games.update(gameId, game);
+    }
+  },
+  submitWinner: function (gameId) {
+    var userId = Meteor.user()._id;
+    var game = Games.findOne(gameId);
+
+    // New Czar
+    var players = Object.keys(game.players);
+    players.forEach(function (player, i) {
+      if (player === userId) {
+        game.currentCzar = players[i + 1] || players[0]; // next or first
+      }
+
+      game.players[player].answer = undefined;
+    });
+
+    // Cleanup
+    game.submittedAnswers = [];
+    game.numAnswers = 0;
+
+    // New Question!
+    game.questions.push(game.question); // Put the current question back
+    game.question = game.questions.shift();
+
+    game.state = "playing";
+    
+    Games.update(gameId, game);
   }
 });
